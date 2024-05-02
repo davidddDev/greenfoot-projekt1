130a1baed95a28979a566f68135cc1a11e56210f
Veka741 <164997511+Veka741@users.noreply.github.com>
2024-05-02 11:31:03 +0200

    spawning enemies

--- Dungeon.java
+++ Dungeon.java
@@ -1,12 +1,17 @@
 import greenfoot.*;
 
 public class Dungeon extends World {
+    private int time;
+    private int killCounter;
     private Player player;
     private HealthBar healthBar;
     public Dungeon(int difficulty, int initialHealth) {
         super(600, 600, 1);
         player = new Player(difficulty);
         addObject(player, getHeight()/2, getWidth()/2);
+        addObject(new Monster(), 500, 600);
+        addObject(new Monster(), 100, 300);
+        addObject(new Monster(), 50, 50);
     }
 
     public Dungeon(int initialHealth) {
@@ -16,8 +21,27 @@ public class Dungeon extends World {
     }
 
     public void act() {
-        
+        time++;
+        spawnEnemies();
+        complete();
         }
+        
+    public void spawnEnemies()
+    {
+        if (time == 300)
+        {
+            addObject(new Monster(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(600));
+            time = 100;
+        }
+    }
+    
+    public void complete()
+    {
+        if (killCounter == 5)
+        {
+            Greenfoot.setWorld(new Dungeon2());
+        }
+    }
 
     public Player getPlayer() {
         return player;
