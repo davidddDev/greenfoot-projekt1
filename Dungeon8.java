e7ee7920e0869e3ae6ed78ac20ba2dd34e5f9057
Veka741 <164997511+Veka741@users.noreply.github.com>
2024-05-13 22:23:20 +0200

    added all dungeons
    (5-10)

---
+++ Dungeon8.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dungeon8 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dungeon8 extends World
+{
+
+    /**
+     * Constructor for objects of class Dungeon8.
+     * 
+     */
+    public Dungeon8()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
---
+++ Dungeon9.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dungeon9 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dungeon9 extends World
+{
+
+    /**
+     * Constructor for objects of class Dungeon9.
+     * 
+     */
+    public Dungeon9()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
