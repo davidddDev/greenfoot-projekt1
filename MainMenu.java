1f9eb883449e2c1561fc266dd9c2698a60e68e15
davidddDev <david08.andrysek@seznam.cz>
2024-05-20 20:10:04 +0200

    controls
    timto by mela byt hra dodelana

--- MainMenu.java
+++ MainMenu.java
@@ -2,8 +2,8 @@ import greenfoot.*;
 
 public class MainMenu extends World {
     
-    public MainMenu() { 
-        super(600, 600, 1); 
+    public MainMenu() {
+        super(600, 600, 1);
         prepare();
     }
     
@@ -11,5 +11,23 @@ public class MainMenu extends World {
         addObject(new Button("Easy", 1), 300, 300);
         addObject(new Button("Medium", 2), 300, 350);
         addObject(new Button("Hard", 3), 300, 400);
+        addObject(new ControlsButton(), 300, 450);
+    }
+}
+
+class ControlsButton extends Actor {
+    public ControlsButton() {
+        updateImage();
+    }
+
+    private void updateImage() {
+        GreenfootImage image = new GreenfootImage("Controls", 25, new Color(193, 133, 98, 200), new Color(0, 0, 0, 0));
+        setImage(image);
+    }
+
+    public void act() {
+        if (Greenfoot.mouseClicked(this)) {
+            Greenfoot.setWorld(new Controls());
+        }
     }
 }
